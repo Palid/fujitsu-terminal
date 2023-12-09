73852d96542be8616f58bd13a8c84961f5de0d39
Dariusz Niemczyk <palid@hackerspace.pl>
2023-12-09 21:31:48 +0100

    Implement or something idk dont ask me

---
+++ src/actions/update-terminal.ts
@@ -0,0 +1,37 @@
+"use server";
+
+function buildUri(line1: string, line2: string) {
+  const uri = process.env.TERMINAL_URI ?? "";
+  if (!uri) {
+    throw new Error("TERMINAL_URI is not set");
+  }
+  const url = new URL(uri);
+
+  url.searchParams.append("line1", line1);
+  url.searchParams.append("line2", line2);
+  return url.toString();
+}
+
+export async function updateTerminalFromLines(formData: FormData) {
+  const line1 = formData.get("line1") ?? "";
+  const line2 = formData.get("line2") ?? "";
+  if (typeof line1 !== "string" || typeof line2 !== "string") {
+    throw new Error("line1 and line2 must be strings");
+  }
+  const uri = buildUri(line1, line2);
+  return fetch(uri, {
+    method: "POST",
+  });
+}
+
+export async function updateTerminalFromCannedResponses(formData: FormData) {
+  const cannedResponse = formData.get("cannedResponse") ?? "";
+  if (typeof cannedResponse !== "string") {
+    throw new Error("cannedResponse must be string");
+  }
+  const [line1, line2] = cannedResponse.split(",");
+  const uri = buildUri(line1, line2);
+  return fetch(uri, {
+    method: "POST",
+  });
+}
